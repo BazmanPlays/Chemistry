eb22a96700f14be7b9465bb78b409bef7b90cdae
BazmanPlays <38334655+BazmanPlays@users.noreply.github.com>
2019-12-13 19:49:08 -0500

    Update SigFig.java

--- chemistry/SigFig.java
+++ chemistry/SigFig.java
@@ -1,5 +1,29 @@
-package chemistry;
-
-public class SigFig {
-
+public class SigFig
+{
+	public SigFig() {
+		
+	}
+	
+	public static int sigFigCount(double input){
+	    String inStr = String.valueOf(input);
+	    int sigFig = 0;
+	    boolean contain = inStr.contains(".");
+	    if(contain && (inStr.charAt(1) != '.')) return inStr.length() - 1;
+	    else if((int)Math.floor(input) == input){
+    	        inStr = "" + (int)Math.floor(input);
+    	        for(int i = 0; i < inStr.length(); i++){
+    	        if(inStr.charAt(i) != '0'){
+    	            sigFig++;
+    	        }
+    	    }
+	    }
+	    else if(inStr.charAt(1) == '.'){
+	        for(int i = 2; i < inStr.length(); i++){
+	            if(inStr.charAt(i) != '0'){
+	                return inStr.length() - i;
+	            }
+	        }
+	    }
+	    return sigFig;
+	}
 }
